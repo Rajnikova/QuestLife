6784738a76f5e312b17d56ee8f3fccff6d7ece5e
Rajnikova <maria.rajnikova@gmail.com>
2019-05-04 21:55:23 +0200

    created quests page and deleting quests

--- src/app/controllers/created_quests_controller.rb
+++ src/app/controllers/created_quests_controller.rb
@@ -1,2 +1,11 @@
 class CreatedQuestsController < ApplicationController
+  def index
+    @my_quests = current_user.quests.where(users_quests: { status: '0' }).order('created_at').paginate(page: params[:page], per_page: 7)
+  end
+
+  def delete
+    quest = Quest.find(params[:quest_id])
+    quest.users_quests.delete
+    quest.delete
+  end
 end
--- src/config/routes.rb
+++ src/config/routes.rb
@@ -3,15 +3,17 @@ Rails.application.routes.draw do
   get 'quests/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  post   '/sign_up',      to: 'sign_up#create'
-  get    '/login',        to: 'login#new'
-  post   '/login',        to: 'login#create'
-  delete '/logout',       to: 'login#destroy'
-  get    '/quests',       to: 'quests#index'
-  patch  '/quests',       to: 'quests#complete'
-  post   '/quests',       to: 'quests#complete'
-  post   '/allquests',    to: 'allquests#new'
-  get    '/quest_detail', to: 'quest_detail#index'
+  post   '/sign_up',       to: 'sign_up#create'
+  get    '/login',         to: 'login#new'
+  post   '/login',         to: 'login#create'
+  delete '/logout',        to: 'login#destroy'
+  get    '/quests',        to: 'quests#index'
+  patch  '/quests',        to: 'quests#complete'
+  post   '/quests',        to: 'quests#complete'
+  post   '/allquests',     to: 'allquests#new'
+  get    '/quest_detail',  to: 'quest_detail#index'
+  get    '/created_quests',to: 'created_quests#index'
+  delete '/created_quests',to: 'created_quests#delete'
   resources :quests do
     post :accept
   end
@@ -21,6 +23,7 @@ Rails.application.routes.draw do
   resources :sign_up
   resources :home
   resources :quest_detail
+  resources :created_quests
   resources :allquests do
     post :accept
   end
