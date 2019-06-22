49fccceb34149329f94379e85c1e00037a056280
Rajnikova <maria.rajnikova@gmail.com>
2019-06-22 10:02:44 +0200

    not 2 same quests in one room

--- app/controllers/admin_rooms_controller.rb
+++ app/controllers/admin_rooms_controller.rb
@@ -8,22 +8,34 @@ class AdminRoomsController < ApplicationController
     @quest_id = params[:quest_id]
   end
   def add_to_room
-    quest_id = params[:quest_id]
-    room = Room.find_by_id(params[:room_id])
-    add = RoomsQuest.new(room_id: room.id,
-                         quest_id: quest_id)
-    if add.save!
-      flash[:success] = (t :quest_added, scope: :flash)
-    else
-      flash[:error] = (t :quest_not_added, scope: :flash)
-    end
+    ActiveRecord::Base.transaction do
+      quest_id = params[:quest_id]
+      room = Room.find_by_id(params[:room_id])
 
-    room.users.each do |user|
-      notification = UsersQuest.new(user_id: user.id,
-                                    quest_id: quest_id,
-                                    status: 10)
-      notification.save
+      exist = room.quests.where(quests: {id: quest_id}).count
+      if exist > 0
+        flash[:error] = (t :quest_is_added, scope: :flash)
+        raise ActiveRecord::Rollback, "quest already exist"
+      end
+
+      add = RoomsQuest.new(room_id: room.id,
+                           quest_id: quest_id)
+
+      if add.save!
+        flash[:success] = (t :quest_added, scope: :flash)
+      else
+        flash[:error] = (t :quest_not_added, scope: :flash)
+        raise ActiveRecord::Rollback, "quest not added"
+      end
+
+      room.users.each do |user|
+        notification = UsersQuest.new(user_id: user.id,
+                                      quest_id: quest_id,
+                                      status: 10)
+        notification.save
+      end
     end
     redirect_back(fallback_location: allquests_path)
+
   end
 end
